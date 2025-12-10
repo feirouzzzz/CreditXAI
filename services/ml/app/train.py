da9ee2a8cfed8112065ef9011c84a930fdd96787
feirouzzzz <feirouzelmouhtadi@gmail.com>
2025-12-10 17:02:37 +0100

    infra

---
+++ services/ml/app/train.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+df = pd.read_csv("dataset.csv")
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+joblib.dump(model, "app/model.joblib")
